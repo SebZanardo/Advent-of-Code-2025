10278915e3c436f5dd81b9ef899948717d5901e3
Seb Zanardo <sebzanardo@optusnet.com.au>
2025-12-01 19:00:33 +1100

    Ran setup script to initialise empty folders with empty template and input files

---
+++ solutions/12/main.py
@@ -0,0 +1,17 @@
+import sys
+
+
+# Try to read a path to an input file from command line arguments
+path = sys.argv[1] if len(sys.argv) > 1 else "input.in"
+
+
+# Parse input
+with open(path, "r") as f:
+    for line in f.readlines():
+        pass
+
+# Part 1
+print(f"Part 1:\n{None}\n")
+
+# Part 2
+print(f"Part 2:\n{None}\n")
